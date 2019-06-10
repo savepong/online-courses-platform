9d413744ac7dd03a47afc9a6d0da2a94080da794
Miss Sai <sai.watcharaphon@gmail.com>
2019-06-10 23:07:42 +0700

    Added HeaderComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,18 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 require('./bootstrap');
 window.Vue = require('vue');
 
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
 
 import IndexPage from './pages/IndexPage'
 // import ArticlesPage from './pages/ArticlesPage'
 
 
-Vue.component('articles-page', require('./pages/ArticlesPage.vue').default);
+Vue.component('header-component', require('./components/HeaderComponent.vue').default);
+
+Vue.use(VueRouter)
+
 
 const router = new VueRouter({
     mode: 'history',
@@ -30,26 +26,9 @@ const router = new VueRouter({
 });
 
 
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('index-page', require('./pages/IndexPage.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 const app = new Vue({
     el: '#app',
+    components: {
+        'index-page': IndexPage 
+    },
 });
