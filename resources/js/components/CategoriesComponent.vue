bf1f4f9f0c37f43c30a38d910bd359b0b2dc4625
Miss Sai <sai.watcharaphon@gmail.com>
2019-06-20 01:12:17 +0700

    Added category

--- resources/js/components/CategoriesComponent.vue
+++ resources/js/components/CategoriesComponent.vue
@@ -1,24 +1,22 @@
 <template>
-    <div> 
-        <h3 class="uk-text-light"> Popular Categories </h3>
-        <div class="uk-grid-small uk-child-width-1-6 uk-margin-large-bottom uk-visible@s" uk-grid>
-            <div v-for="category in categories" :key="category.id">
-                <div class="uk-card-default uk-padding-small">
-                    {{category.title}} 
-                </div>                         
-            </div>
-        </div>
+  <div>
+    <h3 class="uk-text-light">หมวดหมู่ทั้งหมด</h3>
+    <div class="uk-grid-small uk-child-width-1-6 uk-margin-large-bottom uk-visible@s" uk-grid>
+      <div v-for="category in categories" :key="category.id">
+        <div class="uk-card-default uk-padding-small">{{category.title}}</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['categories'],
+  props: ["categories"],
 
-    data() {
-        return {
-            // categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        }
-    }
-}
+  data() {
+    return {
+      // categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    };
+  }
+};
 </script>
